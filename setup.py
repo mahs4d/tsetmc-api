f3d055072fcc0b3219335b89256921fab83d98ef
Mahdi Sadeghi <mail2mahsad@gmail.com>
2020-07-28 19:39:44 +0430

    version change

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ from setuptools import setup
 
 
 def get_directory_files(dir):
-    listOfFile = os.listdir(dir)
-    allFiles = list()
-    for entry in listOfFile:
-        fullPath = os.path.join(dir, entry)
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + get_directory_files(fullPath)
+    files_list = os.listdir(dir)
+    all_files = list()
+    for entry in files_list:
+        full_path = os.path.join(dir, entry)
+        if os.path.isdir(full_path):
+            all_files = all_files + get_directory_files(full_path)
         else:
-            allFiles.append(fullPath)
+            all_files.append(full_path)
 
-    return allFiles
+    return all_files
 
 
 data_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), './lib/tsetmc_api/data')
 
 setup(
     name='tsetmc-api',
-    version='1.3.0',
+    version='1.3.1',
     package_dir={'': 'lib'},
     packages=['tsetmc_api'],
     package_data={'tsetmc_api': get_directory_files(data_dir_path)},
