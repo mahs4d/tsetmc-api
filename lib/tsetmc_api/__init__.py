0c47a5bcbc020021b40d9e4e772a3afe68c4d737
Mahdi Sadeghi <mail2mahsad@gmail.com>
2020-11-20 20:44:46 +0330

    version info

--- lib/tsetmc_api/__init__.py
+++ lib/tsetmc_api/__init__.py
@@ -2,3 +2,5 @@ from .group import SymbolGroup
 from .symbol import Symbol
 from .watch import Watch, Filter, AtiFilter, SahamFilter, WatchTick, SandoghFilter, KalaForoushFilter, \
     EkhtiarForoushFilter, HaghTaghaddomFilter, OraghMosharekatFilter, PayeFarabourseFilter
+
+__version__ = '3.0.4'
