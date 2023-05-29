bbbf353b91aadecddeac61b853ae008354238c74
elyasNz <elyasnz.1999@gmail.com>
2023-05-29 13:03:02 +0330

    added safe_request() and aio_safe_request()

--- lib/tsetmc_api/utils.py
+++ lib/tsetmc_api/utils.py
@@ -1,6 +1,59 @@
+from aiohttp import ClientSession
 from copy import deepcopy
-
 from jdatetime import date as jdate, time as jtime
+from requests import request
+from requests.exceptions import HTTPError
+
+
+def safe_request(method, url, timeout=20, **kwargs):
+    res = request(method.upper(), url, timeout=timeout, **kwargs)
+    res.raise_for_status()
+    return res
+
+
+async def aio_safe_request(method, url, timeout=20, **kwargs):
+    if 'verify' in kwargs:
+        kwargs['ssl'] = kwargs.pop('verify')
+    else:
+        kwargs.setdefault('ssl', True)
+    
+    async with ClientSession() as session:
+        # noinspection PyProtectedMember
+        response = await session._request(method.upper(), url, timeout=timeout, **kwargs)
+        response.text = await response.text()
+        response.close()
+    
+    response.status_code = response.status
+    
+    # region raise_for_status()
+    http_error_msg = ""
+    if isinstance(response.reason, bytes):
+        # We attempt to decode utf-8 first because some servers
+        # choose to localize their reason strings. If the string
+        # isn't utf-8, we fall back to iso-8859-1 for all other
+        # encodings. (See PR #3538)
+        try:
+            reason = response.reason.decode("utf-8")
+        except UnicodeDecodeError:
+            reason = response.reason.decode("iso-8859-1")
+    else:
+        reason = response.reason
+    
+    if 400 <= response.status_code < 500:
+        http_error_msg = (
+            f"{response.status_code} Client Error: {reason} for url: {response.url}"
+        )
+    
+    elif 500 <= response.status_code < 600:
+        http_error_msg = (
+            f"{response.status_code} Server Error: {reason} for url: {response.url}"
+        )
+    
+    if http_error_msg:
+        raise HTTPError(http_error_msg, response=response)
+    # endregion
+    
+    return response
 
 
 def deep_update(d1: dict, d2: dict) -> dict:
