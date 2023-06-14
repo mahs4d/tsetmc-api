72ab172a6a264b585b77a075c8b79f0255f77207
Mahdi Sadeghi <mail2mahsad@gmail.com>
2023-06-14 21:50:52 +0200

    Allow returning multiple thresholds in day details (#14)

--- lib/tsetmc_api/day_details/threshold.py
+++ lib/tsetmc_api/day_details/threshold.py
@@ -1,6 +1,11 @@
+from jdatetime import time as jtime
 from pydantic import BaseModel
 
 
 class DayDetailsThresholdsData(BaseModel):
+    time: jtime | None
     range_max: int
     range_min: int
+
+    class Config:
+        arbitrary_types_allowed = True
